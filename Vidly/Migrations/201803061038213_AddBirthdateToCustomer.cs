818f114c73b73e0cf9469dd9a4c03850ef5d0c7f
Angelo Cappelletti <angelocapp97@gmail.com>
2018-03-06 11:44:42 +0100

    Resolved #1 (Added Birthdate to Customer profile page).

---
+++ Vidly/Migrations/201803061038213_AddBirthdateToCustomer.cs
@@ -0,0 +1,18 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthdateToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Birthdate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Birthdate", c => c.DateTime(nullable: false));
+        }
+    }
+}

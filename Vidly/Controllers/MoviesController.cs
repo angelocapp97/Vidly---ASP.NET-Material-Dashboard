3012194f17505855377bd77b1278201c0549d89e
Angelo Cappelletti <angelocapp97@gmail.com>
2018-03-06 12:32:58 +0100

    Resolved #2 (Added Movie Genres)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,33 +11,28 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private VidlyContext _context;
+
+        public MoviesController()
+        {
+            _context = new VidlyContext();
+        }
 
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
 
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 0, Name = "Shrek" },
-                new Movie { Id = 1, Name = "Hulk" },
-                new Movie { Id = 2, Name = "Batman" },
-                new Movie { Id = 3, Name = "Spiderman" },
-            };
         }
 
         // GET: Movies/Random
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,12 +9,16 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
         public DateTime? Birthdate { get; set; }
     }
 }
